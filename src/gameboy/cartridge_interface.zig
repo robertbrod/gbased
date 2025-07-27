a10c122dae48c5beb6d459a8a7a91ac580121e55
Robert Brod <robbrod93@gmail.com>
2025-07-26 20:14:13 -0500

    Start implementing test and working through cartridge parsing

--- src/gameboy/cartridge_interface.zig
+++ src/gameboy/cartridge_interface.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const CartridgeErrors = error{
+    SampleCartridgeError,
+};
+
 // Example cartridge header:
 // 00000100: 00c3 5001 ceed 6666 cc0d 000b 0373 0083  ..P...ff.....s..
 // 00000110: 000c 000d 0008 111f 8889 000e dccc 6ee6  ..............n.
@@ -10,7 +14,7 @@ const CartridgeHeader = struct {
     const Self = @This();
 
     // 0100-0103 - Entry point
-    entry_point: ?[0x4]u8 = null,
+    entry_point: [0x4]u8 = undefined,
 
     // 0104-0133 - Nintendo logo
     // This area contains a bitmap image that is displayed when the Game Boy is powered on
@@ -18,18 +22,18 @@ const CartridgeHeader = struct {
     // CE ED 66 66 CC 0D 00 0B 03 73 00 83 00 0C 00 0D
     // 00 08 11 1F 88 89 00 0E DC CC 6E E6 DD DD D9 99
     // BB BB 67 63 6E 0E EC CC DD DC 99 9F BB B9 33 3E
-    nintendo_logo: ?[0x30]u8 = null,
+    nintendo_logo: [0x30]u8 = undefined,
 
     // 0134-0143 - Title
     // These bytes contain the title of the game in upper case ASCII.
     // If the title is less than 16 characters long, the remaining bytes should be padded with $00s.
-    cartridge_title: ?[0x10]u8 = null,
+    cartridge_title: [0x10]u8 = undefined,
 
     // 013F-0142 - Manufacturer code
     // In older cartridges these bytes were part of the Title.
     // In newer cartridges they contain a 4-character manufacturer code (in uppercase ASCII).
     // The purpose of the manufacturer code is unknown.
-    manufacturer_code: ?[0x4]u8 = null,
+    manufacturer_code: [0x4]u8 = undefined,
 
     // 0143 - CGB flag
     // In older cartridges this byte was part of the Title.
@@ -38,20 +42,20 @@ const CartridgeHeader = struct {
     //     0x80: The game supports CGB enhancements, but is backwards compatible with monochrome Game Boys
     //     0xC0: The game works on CGB only
     // Setting bit 7 will trigger a write of this register value to KEY0 register which sets the CPU mode.
-    cgb_flag: ?[0x1]u8 = null,
+    cgb_flag: u8 = undefined,
 
     // 0144-0145 - New licensee code
     // This area contains a two-character ASCII “licensee code” indicating the game’s publisher
     // It is only meaningful if the Old licensee is exactly 0x33 otherwise, the old code must be considered.
-    new_licensee_code: ?[0x2]u8 = null,
+    new_licensee_code: [0x2]u8 = undefined,
 
     // 0146 - SGB flag
     // This byte specifies whether the game supports SGB functions.
-    sgb_flag: ?[0x1]u8 = null,
+    sgb_flag: u8 = undefined,
 
     // 0147 - Cartridge type
     // This byte indicates what kind of hardware is present on the cartridge — most notably its mapper.
-    cartridge_type: ?[0x1]u8 = null,
+    cartridge_type: u8 = undefined,
 
     // 0148 - ROM size
     // This byte indicates how much ROM is present on the cartridge.
@@ -67,7 +71,7 @@ const CartridgeHeader = struct {
     // $52  1.1 MiB 72
     // $53  1.2 MiB 80
     // $54  1.5 MiB 96
-    rom_size: ?[0x1]u8 = null,
+    rom_size: u8 = undefined,
 
     // 0149 - RAM size
     // This byte indicates how much RAM is present on the cartridge, if any.
@@ -78,35 +82,58 @@ const CartridgeHeader = struct {
     // $03  32 KiB  4 banks of 8 KiB each
     // $04  128 KiB 16 banks of 8 KiB each
     // $05  64 KiB 8 banks of 8 KiB each
-    ram_size: ?[0x1]u8 = null,
+    ram_size: u8 = undefined,
 
     // 014A - Destination code
     // This byte specifies whether this version of the game is intended to be sold in Japan or elsewhere.
     // $00  Japan (and possibly overseas)
     // $01  Overseas only
-    destination_code: ?[0x1]u8 = null,
+    destination_code: u8 = undefined,
 
     // 014B — Old licensee code
     // This byte is used in older (pre-SGB) cartridges to specify the game’s publisher.
     // However, the value $33 indicates that the New licensee codes must be considered instead.
-    old_licensee_code: ?[0x1]u8 = null,
+    old_licensee_code: u8 = undefined,
 
     // 014C - Mask ROM version number
     // The byte specifies the version number of the game. It is usually $00
-    version_number: ?[0x1]u8 = null,
+    version_number: u8 = undefined,
 
     // 014D - Header checksum
     // This byte contains an 8-bit checksum computed from the cartridge header bytes $0134–014C
-    header_checksum: ?[0x1]u8 = null,
+    header_checksum: u8 = undefined,
 
     // 014E - O14F - Global checksum
-    global_checksum: ?[0x2]u8 = null,
+    global_checksum: [0x2]u8 = undefined,
+
+    pub fn init(cartridge_buffer: []const u8) !Self {
+        var header: Self = .{};
+        @memcpy(header.entry_point, cartridge_buffer[0x100..0x104]);
+    }
 };
 
 const Cartridge = struct {
     const Self = @This();
 
+    alloc: std.mem.Allocator,
+
     header: CartridgeHeader,
+
+    pub fn init(alloc: std.mem.Allocator, cartridge_buffer: []u8) !*Self {
+        const cartridge = try alloc.create(Self);
+        const header = CartridgeHeader.init(cartridge_buffer);
+
+        cartridge.*{
+            .alloc = alloc,
+            .header = header,
+        };
+
+        return cartridge;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.alloc.destroy(self);
+    }
 };
 
 pub fn CartridgeInterface() type {
@@ -114,6 +141,7 @@ pub fn CartridgeInterface() type {
         const Self = @This();
 
         alloc: std.mem.Allocator,
+        cartridge: ?Cartridge = null,
 
         pub fn init(alloc: std.mem.Allocator) !*Self {
             const cartridge_interface = try alloc.create(Self);
@@ -126,11 +154,31 @@ pub fn CartridgeInterface() type {
         }
 
         pub fn deinit(self: *Self) void {
+            if (self.cartridge) |cartridge| {
+                cartridge.deinit();
+            }
+
             self.alloc.destroy(self);
         }
 
-        pub fn readCartridge(catridge_buffer: []u8) void {
-            _ = catridge_buffer;
+        pub fn readCartridge(self: *Self, cartridge_buffer: []u8) void {
+            // Clear out the existing cartridge
+            if (self.cartridge) |cartridge| {
+                cartridge.deinit();
+            }
+
+            self.cartridge = Cartridge.init(self.alloc, cartridge_buffer);
         }
     };
+}
+
+test "Read Cartridge" {
+    const gpa = std.testing.allocator;
+    const cartridge_interface = try CartridgeInterface().init(gpa);
+    defer cartridge_interface.deinit();
+
+    const cartridge_buffer = [0x800000]u8; // 8MB
+    _ = try std.fs.cwd().readFile("../../James Bond 007.gb", cartridge_buffer);
+
+    cartridge_interface.readCartridge(cartridge_buffer);
 }
